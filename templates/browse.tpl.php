20659d386478b508fc5a87b620688cdba931df01
Tiago Grilo Ribeiro Rocha UP202206232 <132365868+TiagoRocha22@users.noreply.github.com>
2024-05-16 22:05:18 +0100

    Update browse.tpl.php

--- templates/browse.tpl.php
+++ templates/browse.tpl.php
@@ -7,57 +7,38 @@
         <link rel="icon" type="image/x-icon" href="../docs/favicon.ico">
     </head>
     <body>
-        <div class="dropdown">
-            <button onclick="myFunction()" class="dropbtn">Price</button>
-            <div id="myDropdown" class="dropdown-content">
-              Less than 500€ <br>
-              500€ - 5000€<br>
-              More than 5000€
+        <header class="website_logo">
+            <div class="top_page">
+            <img class="logo" src="../docs/Logo.jpg" alt="Logo">
+            <a class="nav home" href="../pages/index.html">Home</a>
+            <input type="text" placeholder="Search">
+            <a class="nav messages" href="../pages/messages.html">Messages</a>
+            <a class="cart trans" href="../pages/cart.html"></a>
+            <img class="cart unhovered" src="../docs/shopping_cart_icon.png" alt="Cart">
+            <a class="nav login" href="../pages/login.html">Login</a>
+            <a class="nav register" href="../pages/register.html">Register</a>
             </div>
+        </header>
+        <div class="options">
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item1"></a>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item2"></a>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item3"></a>
+          <br>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item4"></a>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item5"></a>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item6"></a>
+          <br>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item7"></a>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item8"></a>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item9"></a>
+          <br>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item10"></a>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item11"></a>
+          <a href="item.html"><img class="items" src="../docs/placeholder.jpg" alt="Item12"></a>
         </div>
         <section class="wall">
-            <div class="trending">
-                <a href="item.php"><img class="items" src="../docs/placeholder.jpg" alt="Ad1"></a>
-                <a href="item.php"><img class="items" src="../docs/placeholder.jpg" alt="Ad2"></a>
-                <a href="item.php"><img class="items" src="../docs/placeholder.jpg" alt="Ad3"></a>
-            </div>
-            <p class="pop first ad1">Vintage car</p>
-            <p class="pop first ad2">Modern bike</p>
-            <p class="pop first ad3">Popcorn</p>
-            <div class="trending">
-                <a href="item.php"><img class="items" src="../docs/placeholder.jpg" alt="Ad4"></a>
-                <a href="item.php"><img class="items" src="../docs/placeholder.jpg" alt="Ad5"></a>
-                <a href="item.php"><img class="items" src="../docs/placeholder.jpg" alt="Ad6"></a>
-            </div>
-            <p class="pop second ad4">Pocket watch with wheels</p>
-            <p class="pop second ad5">Duck 1</p>
-            <p class="pop second ad6">Duck 2</p>
-            <div class="trending">
-                <a href="item.php"><img class="items" src="../docs/placeholder.jpg" alt="Ad7"></a>
-                <a href="item.php"><img class="items" src="../docs/placeholder.jpg" alt="Ad8"></a>
-                <a href="item.php"><img class="items" src="../docs/placeholder.jpg" alt="Ad"></a>
-            </div>
-            <p class="pop third ad7">Pocket watch with wheels</p>
-            <p class="pop third ad8">Duck 1</p>
-            <p class="pop third ad9">Duck 2</p>
+          <span class="brand">Brand</span>
         </section>
-        <script>
-            function myFunction() {
-              document.getElementById("myDropdown").classList.toggle("show");
-            }
-            window.onclick = function(event) {
-              if (!event.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                  var openDropdown = dropdowns[i];
-                  if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                  }
-                }
-              }
-            }
-            </script>
     </body>
 </html>
 <?php } ?>
