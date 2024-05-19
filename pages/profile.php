9306d197916552f988f2c76b269275a38d84bdbc
TiagoRocha22 <tiagogrocha@icloud.com>
2024-05-19 21:11:18 +0100

    Update

--- pages/profile.php
+++ pages/profile.php
@@ -4,15 +4,20 @@
     require_once(__DIR__ . '/../utils/session.php');
     $session = new Session();
 
-    //require_once(__DIR__ . '/../database/connection.db.php');
+    require_once(__DIR__ . '/../database/connection.php');
     require_once(__DIR__ . '/../database/user.class.php');
     
     require_once(__DIR__ . '/../templates/common.php');
 
     require_once(__DIR__ . '/../templates/profile.tpl.php');
 
+    $userId = $session->getId();
+    $db = getDatabaseConnection();
+
+    $user = User::getUser($db, $userId);
+
     drawHeader($session);
-    drawProfile();
+    drawProfile($user);
     drawFooter();
 ?>
 
